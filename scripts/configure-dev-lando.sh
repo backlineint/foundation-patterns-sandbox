c488b8cdb99d57216548fe2550425ec8350bf468
Brian Perry <contact.brianperry@gmail.com>
2018-03-28 12:40:40 -0500

    Lando: added paragraphs dependency and initial setup script

--- scripts/configure-dev-lando.sh
+++ scripts/configure-dev-lando.sh
@@ -5,9 +5,9 @@ lando start
 lando composer install
 
 # TODO - create synced volumes rather than symlinking
-lando ssh -c "mkdir /app/web/themes/custom"
-lando ssh -c "ln -sf /app/foundation_patterns /app/web/themes/custom/foundation_patterns"
-lando ssh -c "mkdir /app/web/modules/custom"
-lando ssh -c "ln -sf /app/foundation_patterns_config /app/web/modules/custom/foundation_patterns_config"
+lando ssh appserver -c "mkdir /app/web/themes/custom"
+lando ssh appserver -c "ln -sf /app/foundation_patterns /app/web/themes/custom/foundation_patterns"
+lando ssh appserver -c "mkdir /app/web/modules/custom"
+lando ssh appserver -c "ln -sf /app/foundation_patterns_config /app/web/modules/custom/foundation_patterns_config"
 
 lando drush si standard -y --db-url='mysql://drupal8:drupal8@database/drupal8' --site-name='Foundation Patterns' --account-name=admin --account-pass=admin
