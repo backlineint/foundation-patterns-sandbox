5dc3e17ca04ad1edd9d17282f3e21082defbf663
Brian Perry <contact.brianperry@gmail.com>
2018-05-29 14:03:47 -0500

    Front end tooling revisions

--- scripts/configure-dev-lando.sh
+++ scripts/configure-dev-lando.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+cd ../drupal-project
+
+# Clone projects first?
+
 lando start
 
-lando composer install
-
-# TODO - create synced volumes rather than symlinking
-lando ssh appserver -c "mkdir /app/web/themes/custom"
-lando ssh appserver -c "ln -sf /app/foundation_patterns /app/web/themes/custom/foundation_patterns"
-lando ssh appserver -c "mkdir /app/web/modules/custom"
-lando ssh appserver -c "ln -sf /app/foundation_patterns_config /app/web/modules/custom/foundation_patterns_config"
+#lando composer install
 
 lando drush si standard -y --db-url='mysql://drupal8:drupal8@database/drupal8' --site-name='Foundation Patterns' --account-name=admin --account-pass=admin
+
+# Install required modules and themes
+# Todo - install with config instead.
+
+lando drush en -y components ui_patterns
+
+lando drush theme:enable -y zurb_foundation,foundation_patterns
+
+lando drush cset system.theme default foundation_patterns -y
+
+lando drush en -y foundation_patterns_config_marketing
+
+lando drush cr
